d655864d5884a8ce5b5165eaf2447d620cd00a06
Haliris <jeanteissier78@gmail.com>
2024-03-06 18:33:24 +0100

    strnstr done

---
+++ src/ft_strnstr.c
@@ -0,0 +1,36 @@
+static size_t	ft_strlen(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+const char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
+{
+	size_t	i;
+	size_t	needle_i;
+	size_t	needle_len;
+
+	i = 0;
+	needle_i = 0;
+	needle_len = ft_strlen(needle);
+	if (!needle_len || !haystack)
+		return (NULL);
+	while (i < n && haystack[i])
+	{
+		while (haystack[i + needle_i] == needle[needle_i])
+		{
+			needle_i++;
+			if (needle_i == needle_len)
+				return (&haystack[i]);
+		}
+		needle_i = 0;
+		i++;
+	}
+	return (NULL);
+}
