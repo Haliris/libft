6beb6a1be17a1049a9623f3fb3b8c67b227c15bd
Haliris <jeanteissier78@gmail.com>
2024-03-23 18:05:04 +0100

    ft_putnbr_fd normé

---
+++ src/ft_putnbr_fd.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/23 17:59:07 by jteissie          #+#    #+#             */
+/*   Updated: 2024/03/23 18:04:41 by jteissie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putnbr(int n, int fd)
+{
+	long int	number;
+
+	number = n;
+	if (number < 0)
+	{
+		ft_putchar_fd('-', fd);
+		number *= -1;
+	}
+	if (number > 9)
+	{
+		ft_putnbr_fd(number / 10, fd);
+		ft_putchar_fd(number % 10, fd);
+		return ;
+	}
+	ft_putchar_fd(number, fd);
+	return ;
+}
