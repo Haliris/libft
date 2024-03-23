77ee0e32b8b9315354e7d48956a3711d6ce9bcba
Haliris <jeanteissier78@gmail.com>
2024-03-23 16:48:34 +0100

    ft_strlcpy normé

--- src/ft_strlcpy.c
+++ src/ft_strlcpy.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/23 16:47:51 by jteissie          #+#    #+#             */
+/*   Updated: 2024/03/23 16:48:22 by jteissie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 static size_t	ft_strlen(char *str)
 {
@@ -9,7 +20,7 @@ static size_t	ft_strlen(char *str)
 	return (i);
 }
 
-size_t		strlcpy(char *dest, const char *src, size_t size)
+size_t	strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	i;
 
