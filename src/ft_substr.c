606b514813ee9ce60db83876533f698ef1f18fdf
Haliris <jeanteissier78@gmail.com>
2024-03-23 17:38:35 +0100

    ft_substr.c

--- src/ft_substr.c
+++ src/ft_substr.c
@@ -1,12 +1,22 @@
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/23 17:37:49 by jteissie          #+#    #+#             */
+/*   Updated: 2024/03/23 17:38:25 by jteissie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub_str;
-	int	i;
+	int		i;
 
 	i = 0;
-	sub_str = malloc (sizeof(char) * len + 1); //byte null inclu dans len ou non?
+	sub_str = malloc (sizeof(char) * len + 1);
 	if (!sub_str)
 		return (NULL);
 	while (s[start] && i < len)
