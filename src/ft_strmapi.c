2859f9a29cabea727b2e757c62448d0b9fba4c93
Haliris <jeanteissier78@gmail.com>
2024-03-23 16:27:10 +0100

    ft_strmapi done, norme ok

---
+++ src/ft_strmapi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/22 18:36:04 by jteissie          #+#    #+#             */
+/*   Updated: 2024/03/23 16:26:41 by jteissie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static size_t	ft_strlen(char const *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strmapi(char const *s, char (*f) (unsigned int, char))
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!str)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
