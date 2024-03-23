b9b73677150680e343061a9fb1849748748057fa
Haliris <jeanteissier78@gmail.com>
2024-03-23 16:39:25 +0100

    ft_strlcat normé

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -1,15 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/23 16:38:32 by jteissie          #+#    #+#             */
+/*   Updated: 2024/03/23 16:39:13 by jteissie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 static size_t	ft_strlen(char *str)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (str[i])
 		i++;
 	return (i);
-}	
+}
 
-size_t ft_strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	dest_len;
